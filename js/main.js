917fc003a393b36c41c873fd780dc6727b131161
Rodina Adel <112697275+RodinaAdelMahmoud@users.noreply.github.com>
2024-01-25 22:55:21 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+
+$("#open").on('click', function(){
+   $('#menu').animate({width:"250px"},1500)
+   $('#open').animate({marginLeft:'250px'},1500)
+
+})
+$(".closebtn").click(function(){
+$('#menu').animate({width:'0px'},1500)
+$('#open').animate({marginLeft:'0px'},1500)
+
+})
+
+
+// TOGGLE
+
+$('#sliderDown .toggle').click(function(){
+    $('.inner').not($(this).next()).slideUp(500);
+    $(this).next().slideToggle(500);
+});
+
+// COUNTER
+
+function makeTimer() {
+
+		var endTime = new Date("29 April 2020 9:56:00 GMT+01:00");			
+			endTime = (Date.parse(endTime) / 1000);
+
+			var now = new Date();
+			now = (Date.parse(now) / 1000);
+
+			var timeLeft = endTime - now;
+
+			var days = Math.floor(timeLeft / 86400); 
+			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+  
+			if (hours < "10") { hours = "0" + hours; }
+			if (minutes < "10") { minutes = "0" + minutes; }
+			if (seconds < "10") { seconds = "0" + seconds; }
+
+			$("#days").html(days + "<span>Days</span>");
+			$("#hours").html(hours + "<span>Hours</span>");
+			$("#minutes").html(minutes + "<span>Minutes</span>");
+			$("#seconds").html(seconds + "<span>Seconds</span>");		
+
+	}
+
+	setInterval(function() { makeTimer(); }, 1000);
+
+
+		var maxLength =100;
+$('textarea').keyup(function () { 
+	var length = $(this).val().length;
+	var left = maxLength - length;
+	if(left<=0){
+		$('#chars').text("available characters finished");
+		$('#characters').text(" ");
+
+	} else{
+		$('#chars').text(left);
+		$('#characters').text("characters remaining");
+
+	}
+});
+
+
